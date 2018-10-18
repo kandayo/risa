d9b741255ce368d4a574baa76e83f8e7093938d3
kandayo <kandayo@pm.me>
2018-10-18 01:34:18 -0300

    Ignore private messages

--- src/modules/channel_stalker.rb
+++ src/modules/channel_stalker.rb
@@ -4,14 +4,20 @@ module ChannelStalker
   extend Discordrb::EventContainer
 
   channel_create do |event|
+    next if event.channel.private?
+
     Channel.add(event.channel)
   end
 
   channel_update do |event|
+    next if event.channel.private?
+
     Channel[event.channel.id]&.edit(event.channel)
   end
 
   channel_delete do |event|
+    next if event.channel.private?
+
     Channel[event.id]&.deleted!
   end
 end
