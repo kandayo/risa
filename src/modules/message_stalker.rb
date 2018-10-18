7bf003df540e4ba842c2acb3d89f433316e5cd48
kandayo <kandayo@pm.me>
2018-10-18 12:15:09 -0300

    Remove useless conditional

--- src/modules/message_stalker.rb
+++ src/modules/message_stalker.rb
@@ -5,17 +5,14 @@ module Risa
     extend Discordrb::EventContainer
 
     message do |event|
-      next if event.channel.private?
       Message.add(event.message)
     end
 
     message_edit do |event|
-      next if event.channel.private?
       Message[event.message.id]&.edit(event.message)
     end
 
     message_delete do |event|
-      next if event.channel.private?
       Message[event.id]&.wipe
     end
   end
