d90d88d6e10d2d28c110606a7f6f438b508cc586
kandayo <kandayo@pm.me>
2018-10-18 01:20:40 -0300

    Ignore private messages

--- src/modules/message_stalker.rb
+++ src/modules/message_stalker.rb
@@ -4,14 +4,20 @@ module MessageStalker
   extend Discordrb::EventContainer
 
   message do |event|
+    next if event.channel.private?
+
     Message.add(event.message)
   end
 
   message_edit do |event|
+    next if event.channel.private?
+
     Message[event.message.id]&.edit(event.message)
   end
 
   message_delete do |event|
+    next if event.channel.private?
+
     Message[event.id]&.wipe
   end
 end
